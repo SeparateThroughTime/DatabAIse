28c6be10e9572e2f796a9cecded1f6bcdc08a200
SeparateThroughTime <63103971+SeparateThroughTime@users.noreply.github.com>
2026-08-28 15:55:35 +0200

    Repository cleanup
    I want to look like a professional and get rid of this junk that has nothing to do in my repository.

--- Test.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -%(levelname)s -on line: %(lineno)d -%(message)s')
-
-logger = logging.getLogger('name')
-logger.debug('hello')
-logger.warning("warn")
